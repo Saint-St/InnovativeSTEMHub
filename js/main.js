c609582b8a2fb6044a9588a5c0dadb74083cd9f3
SAINT <72196940+Saint-St@users.noreply.github.com>
2025-02-04 12:19:16 +0300

    commitment issues

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ const sr = ScrollReveal({
 })
 /* download cv script*/
 function downloadCV(){
-  window.location.href="file/Dr.Atieno Mutende CV.pdf";
+  window.location.href="file/Dr.AtienoMutendeCV.pdf";
 }
 /* -- HOME -- */
 sr.reveal('.featured-text-card',{})
